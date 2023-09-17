54fb74f3561d0fad495eda4f4b6c9748d78c9f3c
tornikee <tornikexoxiashvili@gmail.com>
2023-09-17 17:30:10 +0400

    change

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.view.PreviewView
 import com.example.myapplication.camera.CameraActivity
 
@@ -20,6 +23,14 @@ class MainActivity : CameraActivity() {
 
         // TODO call this method only after Camera permission will be granted.
         //  in case permission denied show toast message with text: 'Camera permission required for app'
-        launchCamera()
+        requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+
+    }
+    private val requestCameraPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            launchCamera()
+        } else {
+            Toast.makeText(this, "Camera permission required for the app", Toast.LENGTH_SHORT).show()
+        }
     }
 }
